4446a49d48ed6cfdac763c9448acf11990359bf7
Andrew Vu <andrewkvu2@gmail.com>
2024-04-24 17:36:35 -0700

    fix assets directory issue

--- assets/arcade_platformer.tsx
+++ assets/arcade_platformer.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="arcade_platformer" tilewidth="128" tileheight="128" tilecount="42" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="arcade_platformer" tilewidth="1920" tileheight="1351" tilecount="80" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="128" height="128" source="../materials-arcade-platformer/assets/images/ground/Grass/grass.png"/>
@@ -126,5 +126,119 @@
  </tile>
  <tile id="41">
   <image width="128" height="128" source="../materials-arcade-platformer/assets/images/tiles/grass2.png"/>
+ </tile>
+ <tile id="42">
+  <image width="128" height="128" source="images/tiles/png/Tiles/1.png"/>
+ </tile>
+ <tile id="43">
+  <image width="128" height="128" source="images/tiles/png/Tiles/2.png"/>
+ </tile>
+ <tile id="44">
+  <image width="128" height="128" source="images/tiles/png/Tiles/3.png"/>
+ </tile>
+ <tile id="45">
+  <image width="129" height="129" source="images/tiles/png/Tiles/4.png"/>
+ </tile>
+ <tile id="46">
+  <image width="128" height="128" source="images/tiles/png/Tiles/5.png"/>
+ </tile>
+ <tile id="47">
+  <image width="128" height="128" source="images/tiles/png/Tiles/6.png"/>
+ </tile>
+ <tile id="48">
+  <image width="128" height="128" source="images/tiles/png/Tiles/7.png"/>
+ </tile>
+ <tile id="49">
+  <image width="128" height="128" source="images/tiles/png/Tiles/8.png"/>
+ </tile>
+ <tile id="50">
+  <image width="128" height="128" source="images/tiles/png/Tiles/9.png"/>
+ </tile>
+ <tile id="51">
+  <image width="128" height="128" source="images/tiles/png/Tiles/10.png"/>
+ </tile>
+ <tile id="52">
+  <image width="128" height="128" source="images/tiles/png/Tiles/11.png"/>
+ </tile>
+ <tile id="53">
+  <image width="128" height="128" source="images/tiles/png/Tiles/12.png"/>
+ </tile>
+ <tile id="54">
+  <image width="128" height="93" source="images/tiles/png/Tiles/13.png"/>
+ </tile>
+ <tile id="55">
+  <image width="128" height="93" source="images/tiles/png/Tiles/14.png"/>
+ </tile>
+ <tile id="56">
+  <image width="128" height="93" source="images/tiles/png/Tiles/15.png"/>
+ </tile>
+ <tile id="57">
+  <image width="128" height="128" source="images/tiles/png/Tiles/16.png"/>
+ </tile>
+ <tile id="58">
+  <image width="128" height="99" source="images/tiles/png/Tiles/17.png"/>
+ </tile>
+ <tile id="59">
+  <image width="128" height="128" source="images/tiles/png/Tiles/18.png"/>
+ </tile>
+ <tile id="60">
+  <image width="133" height="65" source="images/tiles/png/Object/Bush (1).png"/>
+ </tile>
+ <tile id="61">
+  <image width="133" height="65" source="images/tiles/png/Object/Bush (2).png"/>
+ </tile>
+ <tile id="62">
+  <image width="73" height="47" source="images/tiles/png/Object/Bush (3).png"/>
+ </tile>
+ <tile id="63">
+  <image width="73" height="46" source="images/tiles/png/Object/Bush (4).png"/>
+ </tile>
+ <tile id="64">
+  <image width="77" height="77" source="images/tiles/png/Object/Crate.png"/>
+ </tile>
+ <tile id="65">
+  <image width="49" height="41" source="images/tiles/png/Object/Mushroom_1.png"/>
+ </tile>
+ <tile id="66">
+  <image width="50" height="41" source="images/tiles/png/Object/Mushroom_2.png"/>
+ </tile>
+ <tile id="67">
+  <image width="63" height="65" source="images/tiles/png/Object/Sign_1.png"/>
+ </tile>
+ <tile id="68">
+  <image width="62" height="64" source="images/tiles/png/Object/Sign_2.png"/>
+ </tile>
+ <tile id="69">
+  <image width="90" height="54" source="images/tiles/png/Object/Stone.png"/>
+ </tile>
+ <tile id="70">
+  <image width="116" height="44" source="images/tiles/png/Object/Tree_1.png"/>
+ </tile>
+ <tile id="71">
+  <image width="282" height="301" source="images/tiles/png/Object/Tree_2.png"/>
+ </tile>
+ <tile id="72">
+  <image width="282" height="275" source="images/tiles/png/Object/Tree_3.png"/>
+ </tile>
+ <tile id="73">
+  <image width="1000" height="750" source="images/tiles/png/BG/BG.png"/>
+ </tile>
+ <tile id="74">
+  <image width="128" height="128" source="images/ground/Wood/main_plank.png"/>
+ </tile>
+ <tile id="75">
+  <image width="128" height="128" source="../../../../cool pictures/gutter.png"/>
+ </tile>
+ <tile id="76">
+  <image width="128" height="128" source="images/ground/Wood/half_plank.png"/>
+ </tile>
+ <tile id="77">
+  <image width="1920" height="1351" source="images/bowling_sprites/bowling-background.png"/>
+ </tile>
+ <tile id="78">
+  <image width="1024" height="1024" source="images/bowling_sprites/bowling_background_1.jpg"/>
+ </tile>
+ <tile id="79">
+  <image width="1024" height="1024" source="images/bowling_sprites/bowling_background_2.jpg"/>
  </tile>
 </tileset>
